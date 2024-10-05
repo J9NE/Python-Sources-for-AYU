58f50a7d7be8d2d88aa25e54e4bb42264d2d9b1d
Jin Yoo <138787577+J9NE@users.noreply.github.com>
2024-10-05 21:49:39 +0900

    Update sources of Phonebook_Class.py
    (1) 생성자 함수
    - filename >> filename=""
    - 객체 생성시 텍스트파일이 argument로 전달되지 않아도 argument missing 오류가 발생하지 않게끔 default option 설정함
    (2) PhonebookManager 클래스의 make_phonebook_dict 메소드
    - self.phonebook = {} 의 위치를 생성자에서 해당 메소드로 이동
    - return 구문 추가
    (3) PhonebookManager 클래스의 run 메소드
    - 전화번호부 기능에 해당하는 메소드들을 출력하고 실행하는 if-else 구문을 수정함
    (4) '\t'로 인한 공백이 일정하지 않는 문제를 해결
    (5) 전화번호 유효성 검사 조건으로 isdigit() 조건을 추가함

--- Phonebook_Class.py
+++ Phonebook_Class.py
@@ -1,5 +1,11 @@
+# 전역변수
+name_header = "이름"
+phone_number_header = "전화번호"
+name_header_width = 20
+
 class PhonebookManager:
     def make_phonebook_dict(self):
+        self.phonebook = {}
         try:
             with open(self.filename, 'r', encoding='utf-8') as inputFile:
                 for line in inputFile:
@@ -11,9 +17,8 @@ class PhonebookManager:
             print(f"객체를 생성하는 과정에서 오류가 발생했습니다: {e}\n")
         return self.phonebook
 
-    def __init__(self, filename):
+    def __init__(self, filename=""):
         self.filename = filename
-        self.phonebook = {}
         self.phonebook = self.make_phonebook_dict()
 
     # 전화번호부 관리 프로그램의 기능을 수행하는 메소드
@@ -22,10 +27,10 @@ class PhonebookManager:
         input_name = input("이름을 입력하세요: ")
         if input_name in self.phonebook:
             print("이미 전화번호부에 등록되어 있는 이름입니다!")
-            print(f"{input_name}: {self.phonebook[input_name]}")
+            print(f"{input_name:<{name_header_width}}: {self.phonebook[input_name]}")
             print("전화번호를 수정하고 싶다면, [3. 전화번호 수정] 기능을 사용하세요.")
         else:
-            phone_number = input("전화번호를 입력하세요 (예: 01011110000): ")
+            phone_number = input("전화번호를 입력하세요 (예: 01011110000):")
             # 전화번호 유효성 검사
             if len(phone_number) == 11 and phone_number.startswith('010') and phone_number.isdigit():
                 self.phonebook[input_name] = phone_number
@@ -37,7 +42,9 @@ class PhonebookManager:
     def search_phone_number(self):
         input_name = input("검색할 이름을 입력하세요: ")
         if input_name in self.phonebook:
-            print(f"{input_name}: {self.phonebook[input_name]}")
+            print("검색 결과는 다음과 같습니다.\n"+"-"*40)
+            print(f"{name_header:<{name_header_width}}{phone_number_header}\n"+"-"*40)
+            print(f"{input_name:<{name_header_width}}: {self.phonebook[input_name]}")
         else:
             print("해당 이름이 없습니다.\n")
 
@@ -69,10 +76,11 @@ class PhonebookManager:
 
     # 5. 저장된 전화번호부 목록 확인
     def display_phonebook(self):
-        print("\n저장된 전화번호 목록:")
+        print("전화번호부에 저장된 내용입니다.\n"+"-"*40)
         if self.phonebook:  # self.phonebook가 비어있지 않은 경우
+            print(f"{name_header:<{name_header_width}}{phone_number_header}\n"+"-"*40)
             for name, phone_number in self.phonebook.items():
-                print(f"{name}: {phone_number}")
+                print(f"{name:<{name_header_width}}: {self.phonebook[name]}")
         else:
             print("저장된 전화번호가 없습니다.\n")
 
@@ -82,51 +90,43 @@ class PhonebookManager:
         export_filename = input("저장할 파일의 이름을 작성하세요. (예: classA_export):")
         try:
             with open(export_filename, "w", encoding='utf-8') as export_file:
-                export_file.write("NAME\t\tPHONE NUMBER\n")
-                export_file.write("=" * 30 + "\n")
+                export_file.write(f"{name_header:<{name_header_width}}{phone_number_header}\n"+"="*30+"\n")
                 for name, phone_number in self.phonebook.items():
-                    export_file.write(f"{name}\t\t{phone_number}\n")
+                    export_file.write(f"{name:<{name_header_width}}{phone_number}\n")
             print(f"전화번호부가 \'{export_filename}.txt\'로 내보내졌습니다.\n")
         except Exception as e:
             print(f"파일 추출 중 오류 발생: {e}")
 
     def run(self):
+        options = {
+            '1': ('전화번호 추가', self.add_phone_number),
+            '2': ('전화번호 검색', self.search_phone_number),
+            '3': ('전화번호 수정', self.modify_phone_number),
+            '4': ('전화번호 삭제', self.delete_phone_number),
+            '5': ('저장된 전화번호 확인', self.display_phonebook),
+            '6': ('전화번호부 내보내기', self.export_phonebook),
+            '7': ('프로그램 종료', None)
+        }
+        
         while True:
-            print("\n전화번호 관리 프로그램")
-            print("원하는 기능의 번호를 선택하세요.")
-            print("1. 전화번호 추가")
-            print("2. 전화번호 검색")
-            print("3. 전화번호 수정")
-            print("4. 전화번호 삭제")
-            print("5. 저장된 전화번호 확인")
-            print("6. 전화번호부 내보내기")
-            print("7. 프로그램 종료")
+            print("\n***** 전화번호부 관리 프로그램 *****")
+            print("<Functions>")
+            for num, (description, _) in options.items():
+                print(f"{num}. {description}")
 
-            choice = input("원하는 작업을 선택하세요 (1-7): ")
-
-            if choice == '1':
-                self.add_phone_number()
-            elif choice == '2':
-                self.search_phone_number()
-            elif choice == '3':
-                self.modify_phone_number()
-            elif choice == '4':
-                self.delete_phone_number()
-            elif choice == '5':
-                self.display_phonebook()
-            elif choice == '6':
-                self.export_phonebook()
-            elif choice == '7':
-                print("\n프로그램을 종료합니다.")
-                break
+            user_choice = input("원하는 기능의 번호를 입력하세요(1-7): ")
+            if user_choice in options:
+                if user_choice == '7':
+                    print("\n프로그램을 종료합니다.")
+                    break
+                print("~"*40)
+                options[user_choice][1]()
+                print("~"*40)
             else:
-                print("=" * 50)
-                print("잘못된 입력입니다.\n원하시는 기능의 숫자를 입력해주세요.(1~6).")
-                print("=" * 50)
+                print("*"*40)
+                print("잘못된 입력입니다. (1-7) 사이 숫자를 입력해주세요.")
+                print("*"*40)
 
-
-# 프로그램 실행
-
-fileList = ["PhoneNumbers.txt",]
-A = PhonebookManager(fileList[0])
+# 프로그램 실행 
+A = PhonebookManager("PhoneNumbers.txt")
 A.run()
